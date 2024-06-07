69366f4281b351178941a8c99ab28d9cd1e44b56
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-06-07 20:10:53 -0300

    feat: add node

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, {Request, Response} from "express";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.json)
+
+app.listen(
+    port, 
+    () => {
+        console.log(`server running on port ${port}`)
+    }
+);
