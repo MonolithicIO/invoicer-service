2788fadb1f0fa8d715074521606f9b4e6015c929
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-06-08 01:26:37 -0300

    feat: setup app arch

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
-import express, { Request, Response } from 'express';
-import dotenv from 'dotenv';
+import app from './application/app';
 
-dotenv.config();
-
-const app = express();
 const port = 3000;
-
-app.use(express.json);
 
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
