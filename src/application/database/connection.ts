83b8996ad98651d351640d75c0cb67e49096c581
lucca <lbeurmann.dev@gmail.com>
2024-06-08 19:44:25 -0300

    feat: implement invoice entity

--- src/application/database/connection.ts
+++ src/application/database/connection.ts
@@ -1,16 +1,20 @@
+import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import 'dotenv/config';
+import { InvoiceEntity } from '../../domains/invoices/models/InvoiceEntity';
+import { InvoicePaymentInfoEntity } from '../../domains/invoices/models/InvoicePaymentInfoEntity';
+import { InvoiceServiceEntity } from '../../domains/invoices/models/InvoiceServiceEntity';
 
 export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  //  synchronize: true,
-  logging: true,
-  subscribers: [],
-  migrations: [],
-  entities: [],
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    synchronize: false,
+    logging: true,
+    subscribers: [],
+    migrations: [],
+    entities: [InvoiceEntity, InvoicePaymentInfoEntity, InvoiceServiceEntity]
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import app from './application/app';
-import "reflect-metadata";
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`server running on port ${port}`);
+    console.log(`server running on port ${port}`);
 });
