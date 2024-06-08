051372df33f5ffa30d37c62cf22f1cff1419f30b
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-06-08 01:06:03 -0300

    feat: setup code lint

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,13 @@
-import express, {Request, Response} from "express";
-import dotenv from "dotenv"
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
-app.use(express.json)
+app.use(express.json);
 
-app.listen(
-    port, 
-    () => {
-        console.log(`server running on port ${port}`)
-    }
-);
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
