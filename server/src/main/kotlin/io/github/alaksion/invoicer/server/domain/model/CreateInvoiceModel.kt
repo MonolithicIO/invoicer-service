38a0fa375c50cbcf5c4f93d81efe025859efb8b0
Lucca Beurmann <lbeurmann.dev@gmail.com>
2024-07-04 13:41:05 -0300

    feat: wip invoice dataflow refactor

---
+++ server/src/main/kotlin/io/github/alaksion/invoicer/server/domain/model/CreateInvoiceModel.kt
@@ -0,0 +1,30 @@
+package io.github.alaksion.invoicer.server.domain.model
+
+import kotlinx.datetime.LocalDate
+
+data class CreateInvoiceModel(
+    val externalId: String,
+    val senderCompanyName: String,
+    val senderCompanyAddress: String,
+    val recipientCompanyName: String,
+    val recipientCompanyAddress: String,
+    val issueDate: LocalDate,
+    val dueDate: LocalDate,
+    val beneficiaryName: String,
+    val beneficiaryIban: String,
+    val beneficiarySwift: String,
+    val beneficiaryBankName: String,
+    val beneficiaryBankAddress: String,
+    val intermediaryIban: String? = null,
+    val intermediarySwift: String? = null,
+    val intermediaryBankName: String? = null,
+    val intermediaryBankAddress: String? = null,
+    val activities: List<CreateInvoiceActivityModel>
+)
+
+
+data class CreateInvoiceActivityModel(
+    val description: String,
+    val unitPrice: Long,
+    val quantity: Int
+)
