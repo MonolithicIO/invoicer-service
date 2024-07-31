2bcd67c0170cf2aaf26a4f7295b20d5043b5776e
lucca <lbeurmann.dev@gmail.com>
2024-07-30 22:52:55 -0300

    feat: date filter validation

--- server/src/main/kotlin/io/github/alaksion/invoicer/server/domain/usecase/GetInvoicesUseCase.kt
+++ server/src/main/kotlin/io/github/alaksion/invoicer/server/domain/usecase/GetInvoicesUseCase.kt
@@ -1,8 +1,11 @@
 package io.github.alaksion.invoicer.server.domain.usecase
 
+import io.github.alaksion.invoicer.server.domain.errors.httpError
 import io.github.alaksion.invoicer.server.domain.model.getinvoices.GetInvoicesFilterModel
 import io.github.alaksion.invoicer.server.domain.model.getinvoices.InvoiceListItemModel
 import io.github.alaksion.invoicer.server.domain.repository.InvoiceRepository
+import io.ktor.http.*
+import kotlinx.datetime.LocalDate
 
 internal interface GetInvoicesUseCase {
     suspend fun get(filters: GetInvoicesFilterModel, page: Long, limit: Int): List<InvoiceListItemModel>
@@ -13,7 +16,27 @@ internal class GetInvoicesUseCaseImpl(
 ) : GetInvoicesUseCase {
 
     override suspend fun get(filters: GetInvoicesFilterModel, page: Long, limit: Int): List<InvoiceListItemModel> {
-        // TODO -> Validate filters
+        validateDateFilter(
+            min = filters.minIssueDate,
+            max = filters.maxIssueDate
+        )
+
+        validateDateFilter(
+            min = filters.minDueDate,
+            max = filters.maxDueDate
+        )
         return repository.getInvoices(filters = filters, page = page, limit = limit)
+    }
+
+    private fun validateDateFilter(
+        min: LocalDate?,
+        max: LocalDate?
+    ) {
+        if ((min == null && max != null) || (min != null && max == null)) {
+            httpError(
+                message = "Date filter must contain min and max values.",
+                code = HttpStatusCode.BadRequest
+            )
+        }
     }
 }
