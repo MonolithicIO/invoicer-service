344e3e709698c308c3bf4f3d642f0a540fb68eab
lucca <lbeurmann.dev@gmail.com>
2024-09-03 22:09:33 -0300

    chore: remove legacy user controller

--- server/src/main/kotlin/io/github/alaksion/invoicer/server/view/controller/RootController.kt
+++ server/src/main/kotlin/io/github/alaksion/invoicer/server/view/controller/RootController.kt
@@ -1,5 +1,6 @@
 package io.github.alaksion.invoicer.server.view.controller
 
+import domains.user.controller.userController
 import io.ktor.server.application.Application
 import io.ktor.server.routing.routing
 
--- server/src/main/kotlin/io/github/alaksion/invoicer/server/view/viewmodel/createuser/CreateUserViewModel.kt
+++ server/src/main/kotlin/io/github/alaksion/invoicer/server/view/viewmodel/createuser/CreateUserViewModel.kt
@@ -4,26 +4,5 @@ import io.github.alaksion.invoicer.server.domain.model.user.CreateUserModel
 import kotlinx.serialization.Serializable
 import utils.exceptions.badRequestError
 
-@Serializable
-data class CreateUserRequestViewModel(
-    val email: String? = null,
-    val confirmEmail: String? = null,
-    val password: String? = null,
-)
 
-internal fun CreateUserRequestViewModel.toDomainModel(): CreateUserModel {
-    if (this.email.isNullOrBlank()) badRequestError(message = "E-mail field is required")
-    if (this.password.isNullOrBlank()) badRequestError(message = "password field is required")
-    if (this.confirmEmail.isNullOrBlank()) badRequestError(message = "confirm e-mail field is required")
 
-    return CreateUserModel(
-        email = this.email.trim(),
-        password = this.password.trim(),
-        confirmEmail = this.confirmEmail.trim()
-    )
-}
-
-@Serializable
-data class CreateUserResponseViewModel(
-    val userId: String
-)
