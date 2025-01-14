33c0fa7896a4e36e1d6fecbdebd087863190dd93
Lucca Beurmann <lbeurmann.dev@gmail.com>
2025-01-14 18:30:58 -0300

    feat: move secrets to env

--- foundation/secrets/api/src/main/kotlin/foundation/api/di/SecretsModule.kt
+++ foundation/secrets/api/src/main/kotlin/foundation/api/di/SecretsModule.kt
@@ -1,10 +1,11 @@
 package foundation.api.di
 
-import foundation.api.DotEnvSecretProvider
+import foundation.api.EnvSecretsProvider
 import foundation.api.SecretsProvider
 import org.kodein.di.DI
 import org.kodein.di.bindProvider
+import org.kodein.di.instance
 
 val secretsModule = DI.Module("secrets") {
-    bindProvider<SecretsProvider> { DotEnvSecretProvider }
+    bindProvider<SecretsProvider> { EnvSecretsProvider(environment = instance()) }
 }
