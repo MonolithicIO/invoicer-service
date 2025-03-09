b126d16c5fa03adc236d1bb68eb1fdfef2710b47
Alaksion <lbeurmann.dev@gmail.com>
2025-03-09 00:38:02 -0300

    feat: consume qrcode endpoint

--- controller/src/main/kotlin/controller/UserController.kt
+++ controller/src/main/kotlin/controller/UserController.kt
@@ -3,6 +3,8 @@ package controller
 import controller.viewmodel.createuser.CreateUserRequestViewModel
 import controller.viewmodel.createuser.CreateUserResponseViewModel
 import controller.viewmodel.createuser.toDomainModel
+import foundation.authentication.impl.jwt.jwtProtected
+import foundation.authentication.impl.jwt.jwtUserId
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
@@ -10,10 +12,9 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.kodein.di.instance
 import org.kodein.di.ktor.closestDI
+import services.api.services.qrcodetoken.ConsumeQrCodeTokenService
 import services.api.services.user.CreateUserService
 import services.api.services.user.DeleteUserService
-import foundation.authentication.impl.jwt.jwtProtected
-import foundation.authentication.impl.jwt.jwtUserId
 
 internal fun Routing.userController() {
     route("/v1/user") {
@@ -31,6 +32,18 @@ internal fun Routing.userController() {
             delete {
                 val useCase by closestDI().instance<DeleteUserService>()
                 useCase.delete(jwtUserId())
+                call.respond(HttpStatusCode.NoContent)
+            }
+        }
+
+        jwtProtected {
+            post("v1/user/qrcode/{contentId}/consume") {
+                val qrCodeContentId = call.parameters["contentId"]!!
+                val service by closestDI().instance<ConsumeQrCodeTokenService>()
+                service.consume(
+                    contentId = qrCodeContentId,
+                    userUuid = jwtUserId()
+                )
                 call.respond(HttpStatusCode.NoContent)
             }
         }
