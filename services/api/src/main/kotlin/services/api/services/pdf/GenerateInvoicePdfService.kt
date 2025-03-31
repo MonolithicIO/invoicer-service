f4d7403e58a2a5286a2431c60152496d73dc5f3b
Alaksion <lbeurmann.dev@gmail.com>
2025-03-31 14:16:47 -0300

    feat: store generated pdf path on database

--- services/api/src/main/kotlin/services/api/services/pdf/GenerateInvoicePdfService.kt
+++ services/api/src/main/kotlin/services/api/services/pdf/GenerateInvoicePdfService.kt
@@ -1,10 +1,8 @@
 package services.api.services.pdf
 
-typealias PdfPath = String
-
 interface GenerateInvoicePdfService {
     suspend fun generate(
         invoiceId: String,
         userId: String
-    ): PdfPath
+    )
 }
--- services/impl/src/main/kotlin/services/impl/pdf/GenerateInvoicePdfServiceImpl.kt
+++ services/impl/src/main/kotlin/services/impl/pdf/GenerateInvoicePdfServiceImpl.kt
@@ -1,9 +1,10 @@
 package services.impl.pdf
 
 import io.github.alaksion.invoicer.foundation.storage.FileUploader
+import models.invoicepdf.InvoicePdfStatus
+import repository.api.repository.InvoicePdfRepository
 import services.api.services.invoice.GetInvoiceByIdService
 import services.api.services.pdf.GenerateInvoicePdfService
-import services.api.services.pdf.PdfPath
 import services.api.services.user.GetUserByIdService
 import services.impl.pdf.pdfwriter.InvoicePdfWriter
 import kotlin.io.path.Path
@@ -13,16 +14,19 @@ internal class GenerateInvoicePdfServiceImpl(
     private val getUserByIdService: GetUserByIdService,
     private val getInvoiceByIdService: GetInvoiceByIdService,
     private val writer: InvoicePdfWriter,
-    private val fileUploader: FileUploader
+    private val fileUploader: FileUploader,
+    private val invoicePdfRepository: InvoicePdfRepository
 ) : GenerateInvoicePdfService {
 
-    override suspend fun generate(invoiceId: String, userId: String): PdfPath {
+    override suspend fun generate(invoiceId: String, userId: String) {
         getUserByIdService.get(userId)
 
         val invoice = getInvoiceByIdService.get(
             userId = userId,
             id = invoiceId
         )
+
+        invoicePdfRepository.createInvoicePdf(invoiceId)
 
         val outputPath = Path("")
             .toAbsolutePath()
@@ -33,22 +37,29 @@ internal class GenerateInvoicePdfServiceImpl(
             outputPath = outputPath
         )
 
-        val fileKey = runCatching {
+        runCatching {
             fileUploader.uploadFile(
                 localFilePath = outputPath,
                 fileName = "user/$userId/$invoiceId.pdf"
             )
         }.fold(
             onFailure = {
+                invoicePdfRepository.updateInvoicePdfState(
+                    invoiceId = invoiceId,
+                    status = InvoicePdfStatus.Failed,
+                    filePath = ""
+                )
                 throw it
             },
-            onSuccess = { response ->
-                response
+            onSuccess = { fileKey ->
+                invoicePdfRepository.updateInvoicePdfState(
+                    invoiceId = invoiceId,
+                    status = InvoicePdfStatus.Success,
+                    filePath = fileKey
+                )
             }
         )
 
         Path(outputPath).deleteIfExists()
-
-        return fileKey
     }
 }
