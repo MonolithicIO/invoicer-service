c045e0b1ff6ad924d38ee89e155f603904186b2d
Alaksion <lbeurmann.dev@gmail.com>
2025-04-10 01:43:16 -0300

    feat: make redis connection resilient

--- foundation/cache/src/main/kotlin/foundation/cache/di/CacheDiModule.kt
+++ foundation/cache/src/main/kotlin/foundation/cache/di/CacheDiModule.kt
@@ -4,6 +4,7 @@ import foundation.cache.CacheHandler
 import foundation.cache.redis.RedisCacheHandler
 import foundation.cache.redis.RedisInstance
 import foundation.cache.redis.jedis.JedisRedisManager
+import kotlinx.coroutines.Dispatchers
 import org.kodein.di.DI
 import org.kodein.di.bindProvider
 import org.kodein.di.bindSingleton
@@ -16,5 +17,11 @@ val cacheDiModule = DI.Module("cacheDiModule") {
         )
     }
 
-    bindSingleton<RedisInstance> { JedisRedisManager(secrets = instance()) }
+    bindSingleton<RedisInstance> {
+        JedisRedisManager(
+            secrets = instance(),
+            dispatcher = Dispatchers.IO,
+            logger = instance()
+        )
+    }
 }
