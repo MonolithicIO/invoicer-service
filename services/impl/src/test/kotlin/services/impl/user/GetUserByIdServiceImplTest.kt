accb388a01e19f7981a964a08b824b57fc9ba7d7
Alaksion <lbeurmann.dev@gmail.com>
2025-04-14 22:21:15 -0300

    feat: add missing test

--- services/impl/src/test/kotlin/services/impl/user/GetUserByIdServiceImplTest.kt
+++ services/impl/src/test/kotlin/services/impl/user/GetUserByIdServiceImplTest.kt
@@ -3,9 +3,13 @@ package services.impl.user
 import kotlinx.coroutines.test.runTest
 import models.fixtures.userModelFixture
 import repository.api.fakes.FakeUserRepository
+import utils.exceptions.http.HttpCode
+import utils.exceptions.http.HttpError
 import java.util.*
 import kotlin.test.BeforeTest
 import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 class GetUserByIdServiceImplTest {
     private lateinit var service: GetUserByIdServiceImpl
@@ -30,5 +34,18 @@ class GetUserByIdServiceImplTest {
         val result = repository.getUserByEmail("email@gmail.com")
 
         assert(result == expectedUser)
+    }
+
+    @Test
+    fun `should throw error if user is not found`() = runTest {
+        val error = assertFailsWith<HttpError> {
+            repository.getByIdResponse = { null }
+            service.get(UUID.fromString("123e4567-e89b-12d3-a456-426614174000"))
+        }
+
+        assertEquals(
+            expected = HttpCode.NotFound,
+            actual = error.statusCode
+        )
     }
 }
