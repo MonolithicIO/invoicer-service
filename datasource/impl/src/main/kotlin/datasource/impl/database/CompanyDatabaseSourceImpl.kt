d5b7d0d8e0a8dd4e08340dadf3ff0c6ac21bab2d
Alaksion <lbeurmann.dev@gmail.com>
2025-06-02 22:47:03 -0300

    feat: paged user companies query

--- datasource/impl/src/main/kotlin/datasource/impl/database/CompanyDatabaseSourceImpl.kt
+++ datasource/impl/src/main/kotlin/datasource/impl/database/CompanyDatabaseSourceImpl.kt
@@ -5,11 +5,17 @@ import datasource.api.model.company.CreateCompanyData
 import datasource.api.model.paymentaccount.PaymentAccountType
 import datasource.impl.entities.CompanyAddressTable
 import datasource.impl.entities.PaymentAccountTable
+import datasource.impl.entities.UserCompanyEntity
 import datasource.impl.entities.UserCompanyTable
 import kotlinx.datetime.Clock
+import models.company.CompanyList
+import models.company.CompanyListItem
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+import java.util.*
 
 internal class CompanyDatabaseSourceImpl(
     private val clock: Clock
@@ -68,6 +74,46 @@ internal class CompanyDatabaseSourceImpl(
             companyId.toString().also {
                 commit()
             }
+        }
+    }
+
+    override suspend fun getUserCompanies(
+        userId: UUID,
+        page: Int,
+        limit: Int
+    ): CompanyList {
+        return newSuspendedTransaction {
+            val query = UserCompanyTable
+                .selectAll()
+                .where {
+                    UserCompanyTable.user eq userId and (UserCompanyTable.isDeleted eq false)
+                }
+                .limit(n = limit, offset = (page * limit).toLong())
+
+            val itemCount = query.count()
+
+            val currentOffset = page * limit
+
+            val nextPage = if (itemCount > currentOffset) {
+                (itemCount - currentOffset) / limit
+            } else {
+                null
+            }
+
+            val result = UserCompanyEntity.wrapRows(query)
+                .toList()
+                .map {
+                    CompanyListItem(
+                        name = it.name,
+                        document = it.document
+                    )
+                }
+
+            CompanyList(
+                items = result,
+                nextPage = nextPage,
+                totalCount = itemCount
+            )
         }
     }
 }
