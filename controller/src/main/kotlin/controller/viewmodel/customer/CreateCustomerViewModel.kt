4942d07c9aa0ba85e89ba0937984912c2efa3983
Alaksion <lbeurmann.dev@gmail.com>
2025-06-19 01:27:12 -0300

    feat: implement create customer endpoint

---
+++ controller/src/main/kotlin/controller/viewmodel/customer/CreateCustomerViewModel.kt
@@ -0,0 +1,22 @@
+package controller.viewmodel.customer
+
+import controller.validation.requiredString
+import io.github.alaksion.invoicer.utils.uuid.parseUuid
+import kotlinx.serialization.Serializable
+import models.customer.CreateCustomerModel
+
+@Serializable
+internal data class CreateCustomerViewModel(
+    val name: String? = null,
+    val email: String? = null,
+    val phone: String? = null,
+)
+
+internal fun CreateCustomerViewModel.toModel(
+    companyId: String
+) = CreateCustomerModel(
+    name = requiredString(name, "missing required field: name"),
+    email = requiredString(email, "missing required field: email"),
+    phone = phone,
+    companyId = parseUuid(companyId)
+)
