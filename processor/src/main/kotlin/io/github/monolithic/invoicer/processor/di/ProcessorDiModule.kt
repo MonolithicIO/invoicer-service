3f9cb3bd3ace1a817c63c94de8d13fbb8f5ea864
Alaksion <lbeurmann.dev@gmail.com>
2025-07-20 18:34:38 -0300

    feat: update processor di module

--- processor/src/main/kotlin/io/github/monolithic/invoicer/processor/di/ProcessorDiModule.kt
+++ processor/src/main/kotlin/io/github/monolithic/invoicer/processor/di/ProcessorDiModule.kt
@@ -1,5 +1,7 @@
 package io.github.monolithic.invoicer.processor.di
 
+import io.github.monolithic.invoicer.processor.ProcessBroker
+import io.github.monolithic.invoicer.processor.ProcessConsumer
 import io.github.monolithic.invoicer.processor.ProcessHandler
 import io.github.monolithic.invoicer.processor.context.GeneratePdfCommand
 import io.github.monolithic.invoicer.processor.context.GeneratePdfCommandImpl
@@ -7,6 +9,7 @@ import io.github.monolithic.invoicer.processor.context.context.ProcessCommander
 import io.github.monolithic.invoicer.processor.context.context.ProcessCommanderImpl
 import org.kodein.di.DI
 import org.kodein.di.bindProvider
+import org.kodein.di.bindSingleton
 import org.kodein.di.instance
 
 val processorDiModule = DI.Module(name = "ProcessorDiModule") {
@@ -24,6 +27,17 @@ val processorDiModule = DI.Module(name = "ProcessorDiModule") {
     }
 
     bindProvider<ProcessHandler> {
+        ProcessHandler(
+            logger = instance(),
+            processCommander = instance(),
+            dispatcher = instance()
+        )
+    }
+
+    bindSingleton<ProcessBroker> { instance<ProcessHandler>() }
+    bindProvider<ProcessConsumer> { instance<ProcessHandler>() }
+
+    bindSingleton {
         ProcessHandler(
             logger = instance(),
             processCommander = instance(),
